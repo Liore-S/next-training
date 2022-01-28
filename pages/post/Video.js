c008335e4741a03bc67fd185e28d59a4f4686731
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-01-28 16:45:04 +0700

    playlist link

--- pages/post/Video.js
+++ pages/post/Video.js
@@ -4,12 +4,13 @@ import { YoutubeVideoPlayer } from "../../lib/Yt-embed";
 import { useState } from "react";
 import { Box, Heading, SimpleGrid, Center, Button } from "@chakra-ui/react";
 import Image from "next/image";
+import utilStyles from "../../styles/utils.module.css"
 
 const scrollTop = () => {
   window.scrollTo({ top: 250, behavior: "smooth" });
 };
 
-export default function Video({ results }) {
+export default function Video({ results, results2 }) {
   //console.log(results)
   const [currentVideo, setCurrentVideo] = useState(results[0]);
   const [playing, setPlaying] = useState(false);
@@ -27,15 +28,28 @@ export default function Video({ results }) {
           boxShadow="2xl"
           my={8}
         >
+          {results2 && results2.map((pList) => {
+            return (
+              <div key={pList} className={utilStyles.centered} >
+                <p>Playlist :{" "}
+                  <a className={utilStyles.noUnderlines}
+                    href={`https://www.youtube.com/playlist?list=${process.env.YOUTUBE_PLAYLIST_ID}`}>
+                    {pList.snippet.title}</a></p>
+              </div>
+            )
+          })}
           <YoutubeVideoPlayer
             id={currentVideo.snippet.resourceId.videoId}
             playing={playing}
           ></YoutubeVideoPlayer>
+
+
         </Box>
         <SimpleGrid columns={[1, 2, 3]} spacing={8} my="15">
           {results && results.map((video) => {
             return (
               <Box key={video.id} mx={10}>
+
                 <Image
                   src={video.snippet.thumbnails.maxres?.url}
                   width={1920}
@@ -69,12 +83,18 @@ export default function Video({ results }) {
 export async function getStaticProps() {
   const MY_PLAYLIST = process.env.YOUTUBE_PLAYLIST_ID;
   const API_KEY = process.env.YOUTUBE_API_KEY;
-  const REQUEST_URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${MY_PLAYLIST}&key=${API_KEY}&maxResults=12`;
+  const REQUEST_PLAYLISTLIST = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${MY_PLAYLIST}&key=${API_KEY}&maxResults=12`;
+  const REQUEST_PLAYLIST = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${MY_PLAYLIST}&key=${API_KEY}`
 
-  const response = await fetch(REQUEST_URL);
+  const response = await fetch(REQUEST_PLAYLISTLIST);
   const result = await response.json();
+  const response2 = await fetch(REQUEST_PLAYLIST)
+  const result2 = await response2.json()
   return {
-    props: { results: result.items },
+    props: {
+      results: result.items,
+      results2: result2.items
+    },
     revalidate: 5,
   };
 }
